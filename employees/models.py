cefc1103eaea9cce5eed2a2951aa50ea913f8643
Pallavimishrapm <pmpallavi812@gmail.com>
2023-10-05 07:27:32 +0530

    employee directory

--- employees/models.py
+++ employees/models.py
@@ -2,12 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Employee(models.Model):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    )
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     photo = models.ImageField(upload_to='images')
     designation = models.CharField(max_length=100)
     email_address = models.EmailField(max_length=100, unique=True)
     phono_number = models.CharField(max_length=12, blank=True)
+    gender = models.CharField(max_length=1,choices=GENDER_CHOICES, default='M')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
